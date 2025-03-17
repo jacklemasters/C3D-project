4b77f375213ff40ea06c32f245ce50c8a8558f57
Eric Steil III <eric.steil@concept3d.com>
2025-03-17 18:27:55 +0000

    Add event, place models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+place1 = Place.find_or_create_by!(name: "Test Place") do |place|
+  place.attributes = {
+    address: "123 Main St"
+  }
+end
+
+place2 = Place.find_or_create_by!(name: "Another Place") do |place|
+  place.attributes = {
+    address: "456 Side St"
+  }
+end
+
+Event.find_or_create_by!(name: "Test Event") do |event|
+  event.attributes = {
+    description: "Test Description",
+    starts_at: Time.current + 1.day,
+    ends_at: Time.current + 1.day + 1.hour,
+    place: place1
+  }
+end
+
+Event.find_or_create_by!(name: "Another Event") do |event|
+  event.attributes = {
+    description: "Another Description",
+    starts_at: Time.current + 2.days + 1.hour,
+    ends_at: Time.current + 2.days + 2.hours,
+    place: place2
+  }
+end
+
+Event.find_or_create_by!(name: "Earlier Event") do |event|
+  event.attributes = {
+    description: "This event has already happened",
+    starts_at: Time.current - 1.day,
+    ends_at: Time.current - 1.day + 1.hour,
+    place: place1
+  }
+end
+
+Event.find_or_create_by!(name: "Later Event") do |event|
+  event.attributes = {
+    description: "This event will happen later",
+    starts_at: Time.current + 1.day + 5.hour,
+    ends_at: Time.current + 1.day + 6.hours,
+    place: place2
+  }
+end
